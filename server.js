cd2e07e3e5d65b5150b1dbdd82e30c7cb8c52de5
Chaitanyarana02 <chaitanyaranaa02@gmail.com>
2025-12-09 19:53:59 +0530

    changes

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const { createServer } = require('http');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3002;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
