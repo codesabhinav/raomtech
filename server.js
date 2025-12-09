b5d7e7a2060f2866da702a6765086116e5511c9b
Chaitanyarana02 <chaitanyaranaa02@gmail.com>
2025-12-09 20:39:57 +0530

    chanhes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const PORT = process.env.PORT || 3002;
+const PORT = 4000;
 
 app.prepare().then(() => {
   createServer((req, res) => {
